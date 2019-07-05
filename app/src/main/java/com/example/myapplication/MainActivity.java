dffff185198695f0c44840c2616718b90e200613
John Arnokouros <johnarn@windowslive.com>
2019-07-06 02:28:41 +0300

    Added Listeners for ViewPager and NavigationView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,35 +2,89 @@ package com.example.myapplication;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.myapplication.View.HomeFragment;
+import com.example.myapplication.View.MapFragment;
+import com.example.myapplication.View.MyPagerAdapter;
+import com.example.myapplication.View.SettingsFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+    private HomeFragment homeFragment = new HomeFragment();
+    private SettingsFragment settingsFragment = new SettingsFragment();
+    private MapFragment mapFragment = new MapFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_home:
                         Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(0);
                         break;
-                    case R.id.action_favorites:
-                        Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(1);
                         break;
+
                     case R.id.action_map:
                         Toast.makeText(MainActivity.this, "Map", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(2);
                         break;
                 }
                 return true;
             }
+
         });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    bottomNavigationView.setSelectedItemId(R.id.action_home);
+                } else if (position == 1) {
+                    bottomNavigationView.setSelectedItemId(R.id.action_settings);
+                } else if (position == 2) {
+                    bottomNavigationView.setSelectedItemId(R.id.action_map);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(settingsFragment);
+        adapter.addFragment(mapFragment);
+        viewPager.setAdapter(adapter);
     }
 }
