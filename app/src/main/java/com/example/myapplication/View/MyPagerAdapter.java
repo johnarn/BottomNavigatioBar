9d0c18eeb339939bc040bfcdd2ba86f76e8b6e20
John Arnokouros <johnarn@windowslive.com>
2019-07-06 02:28:05 +0300

    Page Adapter for fragments of navigation menu

---
+++ app/src/main/java/com/example/myapplication/View/MyPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.View;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public MyPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+    public void clearFragment(){
+        mFragmentList.clear();
+    }
+
+}
